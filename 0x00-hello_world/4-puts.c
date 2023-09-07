b8e6fb1dfe7fe378551833eeeaa7a4a0130f09a3
Lawson-JR <lawson5n2010@gmail.com>
2023-09-07 15:21:52 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
+/* This is my Puts.c file */
+
 #include <stdio.h>
+
+/* This main function prints out "Programming is like building a multilingual puzzle" followed by a new line and then returns 0 every time  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
