c6dabe7a4fadc08503c58616f8f080e01919d6bd
Lawson-JR <lawson5n2010@gmail.com>
2023-09-09 05:47:54 -0700

    A-Z or a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+    for(int x = 'a'; x <= 'z'; x++)
+    {
+        putchar(x);
+    }
+    return 0;
+}
