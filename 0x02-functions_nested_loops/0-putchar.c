132dddeb36b8fd2f369dc07e888c0217d0294044
Lawson-JR <lawson5n2010@gmail.com>
2023-09-12 10:48:44 -0700

    COMMITED AGAIN

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - Entry Point
- * Return: Always 0 (Success) 
+ * Return: Always 0(Success) 
  */
 int main(void)
 {
@@ -14,5 +14,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
+	return (0);
 }
