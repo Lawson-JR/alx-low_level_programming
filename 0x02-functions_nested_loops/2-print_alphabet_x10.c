cdcb32d00054c6fe04640858ace818f08737e760
Lawson-JR <lawson5n2010@gmail.com>
2023-09-12 11:19:21 -0700

    letters * 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point.
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char letter;
+
+	while (count < 10)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+		count++;
+	}
+}
