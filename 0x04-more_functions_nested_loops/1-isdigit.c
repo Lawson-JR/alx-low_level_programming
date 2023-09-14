a234b693fd461ce2e49d5ed946f3670fed501fea
Lawson-JR <lawson5n2010@gmail.com>
2023-09-14 13:06:03 -0700

    DIGIT

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Checks for digits.
+ *
+ * Return: 1 if c is a digit, 0 otherwise.
+ */
+int _isdigit(int c)
+{
+	if(c >= 9 && c =< 0){
+		return(1)
+	}
+	else
+		return(0)
+}
