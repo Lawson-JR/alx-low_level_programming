77410dab6b7c1ff2a7da1c33cee116a9746f4af8
Lawson-JR <lawson5n2010@gmail.com>
2023-09-19 12:01:03 -0700

    REVERSED AGAIN

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse.
+ * @s: string to be printed.
+ */
+void print_rev(char *s)
+{
+	int i, j, length;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	length = i;
+
+	for (j = length - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+}
