17e5172e3fd1614496893896737033a36ea8c853
Lawson-JR <lawson5n2010@gmail.com>
2023-09-29 04:41:14 -0700

    REVERSED RECURSIVELY

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse
- * @s: string to be reversed
+ * _print_rev_recursion - prints a string in reverse order or format
+ * @s: string to be printed
  */
 void _print_rev_recursion(char *s)
 {
-	int length = strlen(*s);
-
-    	for (int i = length - 1; i >= 0; i--) {
-        	printf("%c", *s[i]);
-    	}
-
-    	return 0;
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
