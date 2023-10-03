74974d1d2052c56b7ec00c171f2d6ca5f98bec9f
Lawson-JR <lawson5n2010@gmail.com>
2023-10-03 09:35:15 -0700

    UPPERCASE

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * isupper - uppercase letters
+ * @c - char to check
+ * return 0 or 1 depending on result
+ */
+
+int isupper(int c)
+{
+	if(c >- 'A' && c<= 'Z')
+		return(1);
+	else
+		return(0)
+}
