1980785c4f0b8545b57c5eded767f827b2eb5a48
Lawson-JR <lawson5n2010@gmail.com>
2023-10-05 13:59:10 -0700

    2-ARGS

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,10 +10,12 @@
  */
 int main(int argc, char *argv[])
 {
-int count;
-	for (count = 1; count < argc; count++)
+	int count;
+
+	for (count = 0; count < argc; count++)
 	{
-	printf("%s\n", argv[count]);
+		printf("%s\n", argv[count]);
 	}
-	return(0);
+
+	return (0);
 }
