735fc7e335f335f315c591bf0bc983fb82126167
Lawson-JR <lawson5n2010@gmail.com>
2023-10-11 13:35:13 -0700

    ADDED task #0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * @name, type = char *
+ * @age, type = float
+ * @owner, type = char *
+*/
+int main(void)
+{
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+}
