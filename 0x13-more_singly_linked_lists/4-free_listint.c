cbac5c3e7fd971c62ac072e62bce029d088f1213
Lawson-JR <lawson5n2010@gmail.com>
2023-10-24 09:36:20 -0700

    ....

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_ list
+ * @head: listint_t list to be freed
+ */
+
+void free_list(list_t *head)
+{
+        listint_t *temp;
+
+        while (head)
+        {
+                temp = head->next;
+                free(head->str);
+                free(head);
+                head = temp;
+        }
+}
